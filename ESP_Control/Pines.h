df15f5705230fd8bcada902db46785d4265e5142
MoyDC <moises.moy.100@gmail.com>
2024-04-18 16:05:51 -0600

    Pines for GPS sensor and Leds 1, 2, 3 y 4 has been added

--- ESP_Control/Pines.h
+++ ESP_Control/Pines.h
@@ -6,6 +6,10 @@
   #define TX_LidarSensor 19
   SoftwareSerial mySerial_LidarSensor(RX_LidarSensor, TX_LidarSensor); // Crea una instancia de SoftwareSerial
 
+//Sensor GPS
+  #define RX_GPS 18
+  #define TX_GPS 4
+
 //UART0 - Serial
   #define RX0 3
   #define TX0 1
@@ -26,6 +30,12 @@
   #define ENA 14
   #define IN1 27
   #define IN2 26
+
+//Leds
+  #define LED1_FD 13
+  #define LED2_FI 12
+  #define LED3_AD 33
+  #define LED4_AI 32
 
 
 
