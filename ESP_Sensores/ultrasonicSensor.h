dbf916078ccf29b0c87f3c12ceac60047f681c47
MoyDC <moises.moy.100@gmail.com>
2024-04-21 18:43:57 -0600

    Ultrasonic Sensor has been change to be able to use external interrupts instead of pulseIn funtion

--- ESP_Sensores/ultrasonicSensor.h
+++ ESP_Sensores/ultrasonicSensor.h
@@ -1,5 +1,68 @@
-//Class for ultrasonic sensors
+//-----------------------------------------------------------------------------
+//Class to read ultrasonic sensors using external interrupts
 class UltrasonicSensor {
+  private:
+    int triggerPin;
+    int echoPin;
+    volatile unsigned long startCounting;
+    volatile unsigned long stopCounting;
+    volatile unsigned long duration;
+    volatile float distance;
+    volatile bool continuar;
+    volatile bool InterrupcionEstado;
+
+  public:
+    // Constructor sin parámetros
+    UltrasonicSensor() : triggerPin(-1), echoPin(-1) {
+      continuar = true;
+      InterrupcionEstado = false;
+    }
+
+    void SetPines(int trigger, int echo, void (*nameFuntion)(), int modeEchoPin = INPUT){
+      triggerPin = trigger;
+      echoPin = echo;
+      pinMode(triggerPin, OUTPUT);
+      pinMode(echoPin, modeEchoPin);
+      attachInterrupt(digitalPinToInterrupt(echoPin), nameFuntion, CHANGE);
+      continuar = true;
+      InterrupcionEstado = false;
+    }
+    void SetStartCounting(volatile unsigned long _StartCounting_) { startCounting = _StartCounting_; }
+    void SetStopCounting(volatile unsigned long _StopCounting_) { stopCounting = _StopCounting_; }
+    void SetInterruption(volatile bool _InterrupcionEstado_) { InterrupcionEstado = _InterrupcionEstado_; }
+    void SetContinuar(volatile bool _continuar_) { continuar = _continuar_; }
+    volatile bool GetInterrupcionEstado() const { return InterrupcionEstado; }
+    volatile bool GetContinuar() const { return continuar; }
+    volatile float GetDuration() const { return duration; }
+    volatile float GetDistance() const { return distance; }
+
+    void Activar_Sensor(int _delaylectura_ = 5, unsigned long timeout = 1000000UL){
+      //Recomendable usar esta funcion al inicio del loop principal
+      if((micros() - startCounting) >= timeout){
+        InterrupcionEstado = false;
+        continuar = true;
+      }
+      if(InterrupcionEstado == false && continuar == true){
+        continuar = false;
+        delay(_delaylectura_); //5 valor ideal
+        digitalWrite(triggerPin, HIGH);
+        delayMicroseconds(10);
+        digitalWrite(triggerPin, LOW);
+      }
+    }
+
+    void CalculateDuration(){
+      duration = stopCounting - startCounting;
+    }
+
+    void CalculateDistance(){
+      distance = (stopCounting - startCounting) / 58.2;
+    }
+};
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
+//Class for ultrasonic sensors
+/*class UltrasonicSensor {
   private:
     int triggerPin;
     int echoPin;
@@ -55,4 +118,5 @@ class UltrasonicSensor {
       }
       return distance;
     }
-};//End class UltrasonicSensor
+};//End class UltrasonicSensor*/
+//-----------------------------------------------------------------------------
